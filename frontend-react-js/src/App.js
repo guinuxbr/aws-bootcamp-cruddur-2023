e5a042a2c401b2262248988b1b7d259026648c0b
guinuxbr <guinuxbr+github@gmail.com>
2023-02-21 22:00:56 +0000

    Fixed some typos.

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/notifications",
-    element: <HomeNotificationsFeedPageFeedPage />
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
