75937fe99003b768d4d6f0c0d1902f71b2fd78df
guinuxbr <guinuxbr+github@gmail.com>
2023-02-28 22:23:43 +0000

    Fixed imports

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,35 +1,31 @@
-from flask import Flask
-from flask import request
-from flask_cors import CORS, cross_origin
+import logging
 import os
+from time import strftime
 
-from services.home_activities import *
-from services.notifications_activities import *
-from services.user_activities import *
-from services.create_activity import *
-from services.create_reply import *
-from services.search_activities import *
-from services.message_groups import *
-from services.messages import *
-from services.create_message import *
-from services.show_activity import *
-
-# HoneyComb ------
+import rollbar
+import rollbar.contrib.flask
+import watchtower
+from flask import Flask, got_request_exception, request
+from flask_cors import CORS, cross_origin
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import \
+    OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import (BatchSpanProcessor,
                                             ConsoleSpanExporter,
                                             SimpleSpanProcessor)
-
-# Rollbar -----
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
-                    
+from services.create_activity import *
+from services.create_message import *
+from services.create_reply import *
+from services.home_activities import *
+from services.message_groups import *
+from services.messages import *
+from services.notifications_activities import *
+from services.search_activities import *
+from services.show_activity import *
+from services.user_activities import *
 
 # Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
